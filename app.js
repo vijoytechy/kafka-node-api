0751379b745d6c8aa0ddff570a2bd48f3b91f2bf
VIJOY T ABRAHAM <vijoytechy@gmail.com>
2024-10-05 20:32:13 +0300

    code changes

--- app.js
+++ app.js
@@ -1,38 +1,32 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const cors = require('cors'); // Import CORS
+const cors = require('cors');
 const { Kafka } = require("@confluentinc/kafka-javascript").KafkaJS;
 const WebSocket = require('ws');
 const app = express();
-const port = 3000; // You can choose any available port
+const port = process.env.PORT || 3000; // Use environment variable for the port
 
-// Middleware to parse JSON requests
 app.use(bodyParser.json());
-app.use(cors()); // Enable CORS for all routes
+app.use(cors());
 
-// Function to generate a random ticket number
 function generateRandomTicketNumber() {
     const characters = '0123456789';
     let ticketNumber = '';
     for (let i = 0; i < 11; i++) {
         ticketNumber += characters.charAt(Math.floor(Math.random() * characters.length));
     }
-    return 'TK' + ticketNumber;  // Prefix with 'TK'
+    return 'TK' + ticketNumber;
 }
 
-// POST endpoint to receive booking details
 app.post('/bookings', async (req, res) => {
     try {
         const bookingDetails = req.body;
 
-        // Modify the booking details
         bookingDetails.status = 'Confirmed';
         bookingDetails.ticketNumber = generateRandomTicketNumber();
 
-        // Produce the updated booking details to Kafka
         await produceBookingUpdate(bookingDetails);
 
-        // Respond with the updated booking details
         res.status(200).json(bookingDetails);
     } catch (error) {
         console.error('Error processing booking:', error);
@@ -40,7 +34,6 @@ app.post('/bookings', async (req, res) => {
     }
 });
 
-// Function to produce booking updates to Kafka
 async function produceBookingUpdate(bookingDetails) {
     const kafka = new Kafka({
         kafkaJS: {
@@ -62,7 +55,7 @@ async function produceBookingUpdate(bookingDetails) {
     await producer.send({
         topic: 'bookings',
         messages: [
-            { value: JSON.stringify(bookingDetails) , key: bookingDetails.pnr},
+            { value: JSON.stringify(bookingDetails), key: bookingDetails.pnr },
         ],
     });
 
@@ -70,7 +63,6 @@ async function produceBookingUpdate(bookingDetails) {
     console.log("Disconnected from Kafka successfully");
 }
 
-// Setup WebSocket server
 const wss = new WebSocket.Server({ port: 8080 });
 wss.on('connection', (ws) => {
     console.log('Client connected');
@@ -80,12 +72,10 @@ wss.on('connection', (ws) => {
     });
 });
 
-// Function to run the Kafka consumer and send messages to WebSocket clients
 async function runConsumer() {
     let consumer;
     let stopped = false;
 
-    // Initialization with fromBeginning set to true
     consumer = new Kafka({
         kafkaJS: {
             brokers: [process.env.BOOTSTRAP_SERVERS],
@@ -99,7 +89,7 @@ async function runConsumer() {
     }).consumer({
         kafkaJS: {
             groupId: 'booking-consumer-group',
-            fromBeginning: true, // Set to true to consume from the beginning
+            fromBeginning: true,
         },
     });
 
@@ -119,7 +109,6 @@ async function runConsumer() {
                 value: msg,
             });
 
-            // Send message to all connected WebSocket clients
             wss.clients.forEach(client => {
                 if (client.readyState === WebSocket.OPEN) {
                     client.send(msg);
@@ -128,7 +117,6 @@ async function runConsumer() {
         },
     });
 
-    // Keeping the consumer running until stopped
     while (!stopped) {
         await new Promise(resolve => setTimeout(resolve, 1000));
     }
@@ -136,10 +124,8 @@ async function runConsumer() {
     await consumer.disconnect();
 }
 
-// Start the Kafka consumer
 runConsumer().catch((error) => console.error('ERROR TO START Kafka consumer', error));
 
-// Start the server
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
 });
